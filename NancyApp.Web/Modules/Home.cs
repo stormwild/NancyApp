b02b1d3c7007ffe52727be0a1a684db419c7abcf
Alexander Torrijos <artorrijos@gmail.com>
2014-12-08 20:49:11 +0800

    NancyApp working

---
+++ NancyApp.Web/Modules/Home.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace NancyApp.Web.Modules
+{
+	public class Home : NancyModule
+	{
+		public Home()
+		{
+			Get["/"] = _ => View["Home", new { Title = "Home", Greeting = "Hi there welcome to my first Nancy application." }];
+		}
+	}
+}
---
+++ NancyApp.Web/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+
+namespace NancyApp.Web
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder builder)
+		{
+			builder.UseNancy();
+		}
+	}
+}
